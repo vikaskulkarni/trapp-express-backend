a7d1462ffe5f0e38fa8a4246a27248808c68b572
Vikas Dayanand Kulkarni <vikas.dayanand.kulkarni@sap.com>
2024-11-11 13:03:22 +0530

    just express

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-app.enableCors();
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
